ada26093cf51271c4a319f126df980e91fbe8d66
aitimate <549384715@qq.com>
2023-08-14 19:22:38 +0800

    update: -f flag

--- main.go
+++ main.go
@@ -13,23 +13,29 @@ import (
 var flags = []cli.Flag{
 	&cli.StringFlag{
 		Name:     "rootdir",
-		Aliases:  []string{"d"},
+		Aliases:  []string{"r", "R"},
 		Usage:    "The path of project or package",
 		Required: true,
 	},
 	&cli.StringSliceFlag{
 		Name:    "excldirs",
-		Aliases: []string{"e"},
+		Aliases: []string{"e", "E"},
 		Usage:   "The excluded dirs",
 	},
 	&cli.StringFlag{
 		Name:    "output",
-		Aliases: []string{"o"},
+		Aliases: []string{"o", "O"},
 		Usage:   "The output path",
+	},
+	&cli.BoolFlag{
+		Name:    "isFlat",
+		Aliases: []string{"f", "F"},
+		Usage:   "default false, If true, make the packages flat",
+		Value:   false,
 	},
 }
 
-//go:generate go run xxx -d D:\Programs\go-puml-gen -e D:\Programs\go-puml-gen\utils
+//go:generate go run main.go -r D:\Programs\Github组织\VegetableDoggies\plantuml-gen-for-go
 func main() {
 	app := &cli.App{
 		Flags: flags,
@@ -38,29 +44,33 @@ func main() {
 		Action: func(cCtx *cli.Context) error {
 			rootdir, err := filepath.Abs(cCtx.String("rootdir"))
 			if err != nil {
-				log.Fatal("[ERROR] Wrong rootdir :", rootdir, err)
+				log.Fatal("\n[ERROR] Wrong rootdir :", rootdir, err)
 			}
 			excldirs := cCtx.StringSlice("excldirs")
 			for i := range excldirs {
 				excldir, err := filepath.Abs(excldirs[i])
 				if err != nil {
-					log.Fatal("[ERROR] Wrong excldir :", excldirs[i], err)
+					log.Fatal("\n[ERROR] Wrong excldir :", excldirs[i], err)
 				}
 				excldirs[i] = excldir
 			}
-			p := puml.NewPortrait(rootdir, excldirs)
+			isFlat := cCtx.Bool("isFlat")
 			output := cCtx.String("output")
 			if output == "" || filepathx.IsDir(output) {
-				output = filepath.Join(rootdir, filepath.Base(rootdir)+time.Now().Format("20060102150405")+".puml")
+				if isFlat {
+					output = filepath.Join(rootdir, filepath.Base(rootdir)+time.Now().Format("20060102150405")+"F.puml")
+				} else {
+					output = filepath.Join(rootdir, filepath.Base(rootdir)+time.Now().Format("20060102150405")+".puml")
+				}
 				log.Printf("The default output path is active : %s\n", output)
 			}
 			file, err := os.Create(output)
 			if err != nil {
-				log.Fatal("[ERROR] Can't Create the file :", output, err)
+				log.Fatal("\n[ERROR] Can't Create the file :", output, err)
 			}
 			defer file.Close()
-			if _, err = file.Write([]byte(p.Puml)); err != nil {
-				log.Fatal("[ERROR] Can't Write to file :", output, err)
+			if _, err = file.Write([]byte(puml.NewPortrait(rootdir, excldirs, isFlat).Puml)); err != nil {
+				log.Fatal("\n[ERROR] Can't Write to file :", output, err)
 			}
 			log.Printf("Success!\n\n>>> Output = %s\n\n", output)
 			return nil
